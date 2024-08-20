8e9fc1545c4a5cd161e5a7d69a6df5d36b3ea985
Dimitri Farida <dimitrius.farida@gmail.com>
2024-08-19 21:33:50 -0400

    changed for custom ollama setup

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 export default 
 {
     "minecraft_version": "1.20.4", // supports up to 1.20.4
-    "host": "127.0.0.1", // or "localhost", "your.ip.address.here"
-    "port": 55916,
+    "host": "192.168.60.240", // or "localhost", "your.ip.address.here"
+    "port": 24012,
     "auth": "offline", // or "microsoft"
     
     "profiles": [
-        "./andy.json",
+        "./profiles/llama.json",
         // add more profiles here, check ./profiles/ for more
         // more than 1 profile will require you to /msg each bot indivually
     ],
